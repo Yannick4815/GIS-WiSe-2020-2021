0e25978cedadea94896c9a6829c33709e22ff58f
Yannick4815 <yannick.reiche@gmail.com>
2021-01-05 11:20:32 +0100

    testform2

--- Aufgabe_3_1/main.js
+++ Aufgabe_3_1/main.js
@@ -20,6 +20,7 @@ var P_3_1Server;
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
+        console.log(_request.url);
         _response.end();
     }
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
