7064b76c53657afa8523789f2a733caef96af9dc
Yannick4815 <yannick.reiche@gmail.com>
2021-01-15 13:29:36 +0100

    findOne

--- Aufgabe_3_1/main.js
+++ Aufgabe_3_1/main.js
@@ -50,7 +50,7 @@ var P_3_1Server;
         await mongoClient.connect();
         console.log("Success2");
         let orders = mongoClient.db("Test").collection("Students");
-        console.log(orders.find().toArray);
+        console.log(orders.findOne);
         orders.insertOne({ vorname: "Test", nachname: "TestNach", matrikel: 123456 });
     }
     console.log("Test");
